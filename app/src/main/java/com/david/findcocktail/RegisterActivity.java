104d436502e5400f61e4de96e284a235936eb394
DavidCalle0192 <64908945+DavidCalle0192@users.noreply.github.com>
2022-06-12 19:57:39 -0500

    Register user firebase

--- app/src/main/java/com/david/findcocktail/RegisterActivity.java
+++ app/src/main/java/com/david/findcocktail/RegisterActivity.java
@@ -1,14 +1,97 @@
 package com.david.findcocktail;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.david.findcocktail.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
+
+    //VIEW
+    Button btn_register;
+    TextInputEditText txt_user;
+    TextInputEditText txt_password;
+    TextInputEditText txt_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        btn_register = findViewById(R.id.btn_Register);
+        txt_user = findViewById(R.id.txt_User);
+        txt_email = findViewById(R.id.txt_Email);
+        txt_password = findViewById(R.id.txt_Password);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
+    }
+
+    private void registerUser() {
+        final String name = txt_user.getText().toString();
+        final String email = txt_email.getText().toString();
+        final String password = txt_password.getText().toString();
+
+        if(!name.isEmpty() && !email.isEmpty() && !password.isEmpty()){
+            if(password.length() >= 6){
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            saveUser(name, email);
+                        }
+                        else{
+                            Toast.makeText(RegisterActivity.this, "No se ha registrado el usuario", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+            else{
+                Toast.makeText(this, "La contraseña debe tener al menos 6 caracteres", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else{
+            Toast.makeText(this, "Por favor diligenciar todos los campos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+     void saveUser(String name, String email) {
+        User user = new User();
+        user.setEmail(email);
+        user.setName(name);
+
+        mDatabase.child("User").push().setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(RegisterActivity.this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Registro Fallido", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
